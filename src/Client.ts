83f812cf957b27e18ca1b620ec8fed97642a2a6e
Karan Kajla <kkajla12@gmail.com>
2022-03-22 11:39:57 -0700

    Add redirectUrl param to createSelfServiceSession and add createAuthorizationSession method

--- src/Client.ts
+++ src/Client.ts
@@ -1,5 +1,5 @@
 import Axios, { AxiosInstance } from "axios";
-import { API_URL_BASE, API_VERSION } from "./constants";
+import { API_URL_BASE, API_VERSION, SELF_SERVICE_DASH_URL_BASE } from "./constants";
 import Permission from "./types/Permission";
 import Role from "./types/Role";
 import Session from "./types/Session";
@@ -210,20 +210,42 @@ export default class Client {
     }
 
     /**
-     * Creates a self-service session in Warrant for the user with the specified userId and returns a session token which can be used to
-     * make authorized requests to the Warrant API scoped to the specified tenantId.
+     * Creates an authorization session in Warrant for the specified userId and returns a session token that can be used to
+     * make authorization requests to the Warrant API scoped to the specified userId and tenantId.
+     *
+     * @param session A session object containing the userId, redirectUrl, and optional tenantId for which the authorization session should be created.
+     * @returns A session token that can be passed to any of the Warrant client-side SDKs to allow the SDK to make client-side authorization checks for the specified user.
+     */
+    public async createAuthorizationSession(session: Session): Promise<string> {
+        try {
+            const response = await this.httpClient.post("/sessions", {
+                ...session,
+                type: "sess",
+            });
+
+            return response.data.token;
+        } catch (e) {
+            console.log("Error creating authorization session for user in Warrant", e.response.data);
+
+            throw e;
+        }
+    }
+
+    /**
+     * Creates a self-service session in Warrant for the user with the specified userId and returns a session token that can be used to
+     * make authorized requests to the Warrant API scoped to the specified userId and tenantId.
      *
      * @param session A session object containing the userId, redirectUrl, and optional tenantId for which the self service session should be created.
      * @returns A url pointing to the self-service dashboard that will allow the specified user to make changes to the roles and permissions of users in their tenant.
      */
-    public async createSelfServiceSession(session: Session): Promise<string> {
+    public async createSelfServiceSession(session: Session, redirectUrl: string): Promise<string> {
         try {
-            const response = await this.httpClient.post(`/sessions`, {
+            const response = await this.httpClient.post("/sessions", {
                 ...session,
                 type: "ssdash",
             });
 
-            return response.data.url;
+            return `${SELF_SERVICE_DASH_URL_BASE}/${response.data.token}?redirectUrl=${redirectUrl}`;
         } catch (e) {
             console.log("Error creating self-service session for user in Warrant", e.response.data);
 
