8e09f9fb92f859d7862702df0492c00e1153140c
Karan Kajla <kkajla12@gmail.com>
2022-07-13 11:41:54 -0700

    Update ApiError definition

--- src/types/ApiError.ts
+++ src/types/ApiError.ts
@@ -1,12 +1,39 @@
 export default class ApiError extends Error {
-    private code: string;
+    public code: string;
+    public message: string;
+    public name: string;
 
     constructor(code: string, message: string) {
         super(message);
         this.code = code;
+        this.message = message;
+        this.name = ApiError.codeToName(code);
     }
 
     public toString(): string {
-        return `Code: ${this.code}, Message: ${this.message}`;
+        return `${this.name} [${this.code}]: ${this.message}`;
+    }
+
+    private static codeToName(code: string): string {
+        switch (code) {
+            case "duplicate_record":
+                return "DuplicateRecordError";
+            case "internal_error":
+                return "InternalError";
+            case "invalid_request":
+                return "InvalidRequestError";
+            case "invalid_parameter":
+                return "InvalidParameterError";
+            case "missing_required_parameter":
+                return "MissingRequiredParameterError";
+            case "not_found":
+                return "NotFoundError";
+            case "unauthorized":
+                return "UnauthorizedError";
+            case "unknown_origin":
+                return "UnknownOriginError";
+            default:
+                return "ApiError";
+        }
     }
 }
