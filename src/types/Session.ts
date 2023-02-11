e9d242cd318c98802dbcbbb27b7291e71eff1d1f
Karan Kajla <kkajla12@gmail.com>
2023-02-11 13:49:01 -0800

    Add ttl and context to SessionParams and create a separate SelfServiceSessionParams for creating self service sessions

--- src/types/Session.ts
+++ src/types/Session.ts
@@ -1,4 +1,12 @@
+import { Context } from "./Warrant";
+
 export interface SessionParams {
     userId: string;
     tenantId?: string;
+    ttl?: number;
+    context?: Context;
+}
+
+export interface SelfServiceSessionParams extends SessionParams {
+    selfServiceStrategy: "basic" | "advanced";
 }
