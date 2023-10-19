5f36b2f7ebddf0b3b6bc3077b1b22bb8070daf3f
stanleyphu <phu.stanley@gmail.com>
2023-10-19 13:48:30 -0700

    Export object and warrant modules

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 export * from "./types";
 export { default as Feature } from "./modules/Feature";
+export { default as Object } from "./modules/ObjectModule";
 export { default as Permission } from "./modules/Permission";
 export { default as PricingTier } from "./modules/PricingTier";
 export { default as Role } from "./modules/Role";
 export { default as Tenant } from "./modules/Tenant";
 export { default as User } from "./modules/User";
+export { default as Warrant } from "./modules/WarrantModule";
 export { default as WarrantClient } from "./WarrantClient";
